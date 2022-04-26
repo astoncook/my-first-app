181a69b3fde706bd73d66d7be175ec9158861ec8
Aston Cook <coo19025@byui.edu>
2022-04-25 20:43:00 -0500

    part of lesson 2

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,21 @@
+import { Component } from "@angular/core";
+import { reduce } from "rxjs";
+
+@Component({
+    selector: 'app-warning-alert',
+    template: `
+        <p>This is a warning, you are in danger!</p>
+    `,
+    styles: [
+        `
+        p {
+            padding: 20px;
+            background-color: mistyrose;
+            border: 1px solid
+        }
+        `
+    ]
+})
+export class WarningAlertComponent{
+
+}
